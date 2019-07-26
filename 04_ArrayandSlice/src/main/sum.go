047af76c9a80894cf88812fe6eb212296595725d
shgan <shgan@mappers.kr>
2019-07-26 14:42:22 +0900

    04_ArrayandSlice 완료

--- 04_ArrayandSlice/src/main/sum.go
+++ 04_ArrayandSlice/src/main/sum.go
@@ -24,3 +24,33 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+
+	//위 코드가 아래와 같이 리팩토링 되면서 slice의 용량에 대한 걱정이 없어진다.
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
+	}
+	return sums
+}
